1d47e51fd3b7f96efa56d3972276f3a08278ed55
CatsLover2006 <chancelorne@icloud.com>
2022-11-29 20:00:21 -0700

    3DS Port Main Version
    Might adapt the Processing version into a level editor

--- 3ds/source/lineList.hpp
+++ 3ds/source/lineList.hpp
@@ -11,55 +11,128 @@ class line {
 };
 
 const line* linelist[] = {
-	new line(0, 200, 160, 180),
-	new line(160, 180, 250, 80),
-	new line(0, 80, 100, 200),
-	new line(250, 100, 100, 200),
-	new line(250, 52, 340, 50),
-	new line(0, 200, 0, 0),
-	new line(340, 50, 350, 0),
-	new line(350, 0, 0, 0)
+	new line(160, 180, 240, 110), // Level 1
+	new line(0, 200, 160, 180), // Level 1
+	new line(0, 150, 100, 200), // Level 1
+	new line(260, 120, 100, 200), // Level 1
+	new line(300, 52, 400, 50), // Level 1
+	new line(0, 200, 0, 0), // Level 1
+	new line(400, 50, 400, 0), // Level 1
+	new line(400, 0, 0, 0), // Level 1
+	new line(280, 95, 240, 110), // Level 1
+	new line(270, 110, 280, 95), // Level 1
+	new line(270, 110, 260, 120), // Level 1
 };
 
-const int levelEnd[] = {
-	8,
-	8
+const line* linelistKO[] = {
+	new line(320, 180, 350, 150), // Level 1, X
+	new line(350, 180, 320, 150), // Level 1, X
+	new line(335, 200, 335, 220), // Level 1, Death Arrow 1
+	new line(325, 210, 335, 220), // Level 1, Death Arrow 1
+	new line(345, 210, 335, 220), // Level 1, Death Arrow 1
+	new line(225 + sqrt(200), 220 - sqrt(200), 225, 220), // Level 1, Death Arrow 2
+	new line(225, 220, 225, 220 - sqrt(200)), // Level 1, Death Arrow 2
+	new line(225 + sqrt(200), 220, 225, 220), // Level 1, Death Arrow 2
+	new line(275, 220, 275 + sqrt(200), 220 - sqrt(200)), // Level 1, Death Arrow 3
+	new line(275 + sqrt(200), 220, 275, 220), // Level 1, Death Arrow 3
+	new line(275, 220 - sqrt(200), 275, 220), // Level 1, Death Arrow 3
+};
+
+const line* linelistBG[] = {
+	new line(90, 20, 80, 50), // Level 1, A Button
+	new line(90, 20, 100, 50), // Level 1, A Button
+	new line(85, 35, 95, 35), // Level 1, A Button
+	new line(90, 10, 65, 35), // Level 1, A Button
+	new line(90, 10, 115, 35), // Level 1, A Button
+	new line(90, 60, 65, 35), // Level 1, A Button
+	new line(90, 60, 115, 35), // Level 1, A Button
+	new line(135, 110, 145, 110), // Level 1, Right Arrow
+	new line(140, 115, 145, 110), // Level 1, Right Arrow
+	new line(140, 105, 145, 110), // Level 1, Right Arrow
+	new line(85, 110, 75, 110), // Level 1, Left Arrow
+	new line(80, 115, 75, 110), // Level 1, Left Arrow
+	new line(80, 105, 75, 110), // Level 1, Left Arrow
+	new line(140, 25, 140, 45), // Level 1, Jump Arrow
+	new line(130, 35, 140, 25), // Level 1, Jump Arrow
+	new line(150, 35, 140, 25), // Level 1, Jump Arrow
+	new line(375, 35, 375, 55), // Level 1, End Arrow
+	new line(365, 45, 375, 35), // Level 1, End Arrow
+	new line(385, 45, 375, 35), // Level 1, End Arrow
+	new line(390, 72.5, 365, 60), // Level 1, End Flag
+	new line(365, 60, 345, 100), // Level 1, End Flag
+	new line(390, 72.5, 380, 92.5), // Level 1, End Flag
+	new line(355, 80, 380, 92.5), // Level 1, End Flag
+	new line(365, 60, 380, 92.5), // Level 1, End Flag
+	new line(355, 80, 390, 72.5), // Level 1, End Flag
+	new line(100, 70, 100, 100), // Level 1, D-Pad
+	new line(70, 100, 100, 100), // Level 1, D-Pad
+	new line(120, 70, 120, 100), // Level 1, D-Pad
+	new line(70, 120, 100, 120), // Level 1, D-Pad
+	new line(100, 70, 120, 70), // Level 1, D-Pad
+	new line(70, 100, 70, 120), // Level 1, D-Pad
+	new line(100, 120, 100, 150), // Level 1, D-Pad
+	new line(120, 100, 150, 100), // Level 1, D-Pad
+	new line(120, 120, 120, 150), // Level 1, D-Pad
+	new line(120, 120, 150, 120), // Level 1, D-Pad
+	new line(100, 150, 120, 150), // Level 1, D-Pad
+	new line(150, 100, 150, 120), // Level 1, D-Pad
 };
 
 const int levelStart[] = {
-	0,
-	1
+	0
 };
+
+const int levelEnd[] = {
+	11
+};
+
+const int koStart[] = {
+	0
+};
+
+const int koEnd[] = {
+	11
+};
+
+const int bgStart[] = {
+	0,
+	24
+};
+
+const int bgEnd[] = {
+	37
+};
+
 
 const float bounds[][4] = {
 	{
 		0,
 		0,
-		4000,
-		3000
+		400,
+		240
 	},
 	{
 		0,
 		0,
-		350,
-		200
+		0,
+		0
 	}
 };
 
 const float startPos[][2] = {
 	{
 		50,
-		124.3
+		163.8
 	},
 	{
-		50,
-		124.3
+		0,
+		0
 	}
 };
 
 const float endPoint[][2] = {
 	{
-		315,
+		375,
 		25
 	},
 	{
@@ -67,3 +140,5 @@ const float endPoint[][2] = {
 		25
 	}
 };
+
+const int levels = 1;
--- 3ds/source/main.cpp
+++ 3ds/source/main.cpp
@@ -5,6 +5,8 @@
 #define SCREEN_WIDTH 400
 #define BOTTOM_SCREEN_WIDTH 320
 #define SCREEN_HEIGHT 240
+#define SUBSTEPS 6
+#define MAX_SPEED 59
 
 #include <citro2d.h>
 
@@ -15,6 +17,9 @@
 #include "lineList.hpp"
 #include "collisions.hpp"
 
+const float xDiv = 1 + (sqrt(6)-1)/SUBSTEPS,
+			xAdd = 2.69/sqrt(SUBSTEPS*sqrt(SUBSTEPS)-pow(SUBSTEPS, xDiv)/(xDiv*sqrt(2)));
+
 float playerX = 50,
     playerY = 50,
     oX = playerX,
@@ -24,22 +29,28 @@ float playerX = 50,
     rot = 0,
     rotSpd = 0,
     cX = playerX,
-    cY = playerY;
+    cY = playerY,
+	animTimer = 0;
 
 const double pi = acos(-1.0);
 
+u8 consoleModel = 3;
+u8 lang = CFG_LANGUAGE_EN;
+u8 serial = 0;
 u8 wideModifier = 2;
 
-static u32 clrWhite, clrBlack;
+static u32 clrWhite, clrBlack, clrCyan, clrRed, clrGrey;
 
 int level = 0,
 	oLvl = -1,
-	curLine;
+	curLine,
+	animID = -1; // -1 not animating
 
 bool vcolCheck = false,
     hcolCheck = false,
 	tcolCheck = false,
-    hasStarted = false;
+    hasStarted = false,
+	dcolCheck = false;
 
 float constrain(float x, float min, float max) {
 	if (x < min) return min;
@@ -47,7 +58,33 @@ float constrain(float x, float min, float max) {
 	return x;
 }
 
+float max (float a, float b) {
+	if (a > b) return a;
+	return b;
+}
+
+float min (float a, float b) {
+	if (a < b) return a;
+	return b;
+}
+
 void drawLevel() {
+	for (curLine = bgStart[level]; curLine < bgEnd[level]; curLine++) {
+		C2D_DrawLine(linelistBG[curLine]->startX - cX + SCREEN_WIDTH / 2,
+					 linelistBG[curLine]->startY - cY + SCREEN_HEIGHT / 2,
+					 clrGrey,
+					 linelistBG[curLine]->endX - cX + SCREEN_WIDTH / 2,
+					 linelistBG[curLine]->endY - cY + SCREEN_HEIGHT / 2,
+					 clrGrey, 2, 0.05f);
+	}
+	for (curLine = bgStart[level]; curLine < bgEnd[level]; curLine++) {
+		C2D_DrawCircleSolid(linelistBG[curLine]->endX - cX + SCREEN_WIDTH / 2,
+							linelistBG[curLine]->endY - cY + SCREEN_HEIGHT / 2,
+							0.05f, 1, clrGrey);
+		C2D_DrawCircleSolid(linelistBG[curLine]->startX - cX + SCREEN_WIDTH / 2,
+							linelistBG[curLine]->startY - cY + SCREEN_HEIGHT / 2,
+							0.05f, 1, clrGrey);
+	}
 	for (curLine = levelStart[level]; curLine < levelEnd[level]; curLine++) {
 		C2D_DrawLine(linelist[curLine]->startX - cX + SCREEN_WIDTH / 2,
 					 linelist[curLine]->startY - cY + SCREEN_HEIGHT / 2,
@@ -56,7 +93,45 @@ void drawLevel() {
 					 linelist[curLine]->endY - cY + SCREEN_HEIGHT / 2,
 					 clrBlack, 2, 0.1f);
 	}
-	C2D_DrawCircleSolid(endPoint[level][0],endPoint[level][1], 0.4f, 1, C2D_Color32(0, 255, 255, 0xFF));
+	for (curLine = levelStart[level]; curLine < levelEnd[level]; curLine++) {
+		C2D_DrawCircleSolid(linelist[curLine]->endX - cX + SCREEN_WIDTH / 2,
+							linelist[curLine]->endY - cY + SCREEN_HEIGHT / 2,
+							0.1f, 1, clrBlack);
+		C2D_DrawCircleSolid(linelist[curLine]->startX - cX + SCREEN_WIDTH / 2,
+							linelist[curLine]->startY - cY + SCREEN_HEIGHT / 2,
+							0.1f, 1, clrBlack);
+	}
+	for (curLine = koStart[level]; curLine < koEnd[level]; curLine++) {
+		C2D_DrawLine(linelistKO[curLine]->startX - cX + SCREEN_WIDTH / 2,
+					 linelistKO[curLine]->startY - cY + SCREEN_HEIGHT / 2,
+					 clrRed,
+					 linelistKO[curLine]->endX - cX + SCREEN_WIDTH / 2,
+					 linelistKO[curLine]->endY - cY + SCREEN_HEIGHT / 2,
+					 clrRed, 2, 0.11f);
+	}
+	for (curLine = koStart[level]; curLine < koEnd[level]; curLine++) {
+		C2D_DrawCircleSolid(linelistKO[curLine]->endX - cX + SCREEN_WIDTH / 2,
+							linelistKO[curLine]->endY - cY + SCREEN_HEIGHT / 2,
+							0.11f, 1, clrRed);
+		C2D_DrawCircleSolid(linelistKO[curLine]->startX - cX + SCREEN_WIDTH / 2,
+							linelistKO[curLine]->startY - cY + SCREEN_HEIGHT / 2,
+							0.11f, 1, clrRed);
+	}
+	C2D_DrawCircleSolid(endPoint[level][0] - cX + SCREEN_WIDTH / 2,
+						endPoint[level][1] - cY + SCREEN_HEIGHT / 2,
+						0.15f, 3, clrBlack);
+	C2D_DrawCircleSolid(endPoint[level][0] - cX + SCREEN_WIDTH / 2,
+						endPoint[level][1] - cY + SCREEN_HEIGHT / 2,
+						0.4f, 2, clrCyan);
+}
+
+void drawLog() {
+	printf("\x1b[1;1HX:       %7.2f%\x1b[K", playerX);
+	printf("\x1b[2;1HY:       %7.2f%\x1b[K", playerY);
+	printf("\x1b[3;1HCPU:     %6.2f%%\x1b[K", C3D_GetProcessingTime()*6.0f);
+	printf("\x1b[4;1HGPU:     %6.2f%%\x1b[K", C3D_GetDrawingTime()*6.0f);
+	printf("\x1b[5;1HCmdBuf:  %6.2f%%\x1b[K", C3D_GetCmdBufUsage()*100.0f);
+	printf("\x1b[6;1HTmr:     %7.2f%\x1b[K", animTimer);
 }
 
 int main(int argc, char* argv[]) {
@@ -71,23 +146,31 @@ int main(int argc, char* argv[]) {
 	// Input Vars
     touchPosition touch;
 	u32 kDown, kHeld, kUp;
+	int jumpableFor = 25;
 	
 	// Create Colors
-    clrWhite = C2D_Color32(255, 255, 255, 0xFF),
+    clrWhite = C2D_Color32(255, 255, 255, 0xFF);
 	clrBlack = C2D_Color32(0, 0, 0, 0xFF);
+	clrCyan = C2D_Color32(0, 255, 255, 0xFF);
+	clrRed = C2D_Color32(255, 0, 0, 0xFF);
+	clrGrey = C2D_Color32(200, 200, 200, 0xFF);
 	
 	// Wide mode
     Result res = cfguInit();
     if (R_SUCCEEDED(res)) {
-        CFGU_GetModelNintendo2DS(&wideModifier);
-        wideModifier = 2 - wideModifier;
+		CFGU_GetSystemLanguage(&lang);
+        CFGU_GetSystemModel(&consoleModel);
+		CFGI_SecureInfoGetSerialNumber(&serial);
         cfguExit();
     }
+    if (consoleModel == 3) wideModifier = 1;
+	if (serial == 0) wideModifier = 1;
     gfxSetWide(wideModifier == 2);
 	
 	// Create Screens
     C3D_RenderTarget * top_main = C2D_CreateScreenTarget(GFX_TOP, GFX_LEFT);
-    C3D_RenderTarget * bottom = C2D_CreateScreenTarget(GFX_BOTTOM, GFX_LEFT);
+    //C3D_RenderTarget * bottom = C2D_CreateScreenTarget(GFX_BOTTOM, GFX_LEFT);
+	consoleInit(GFX_BOTTOM, NULL);
 	
 	// Log
 	std::fstream log;
@@ -96,6 +179,7 @@ int main(int argc, char* argv[]) {
 	// Main Loop
 	while (aptMainLoop())
     {
+		animTimer += 1 / 60.0f;
 		// Input
 		hidScanInput();
         hidTouchRead(&touch);
@@ -105,6 +189,7 @@ int main(int argc, char* argv[]) {
 		
 		// Gameloop
 		if (!hasStarted || oLvl != level) {
+			animTimer = 0;
 			playerX = startPos[level][0];
 			playerY = startPos[level][1];
 			playerYVel = 0;
@@ -116,97 +201,88 @@ int main(int argc, char* argv[]) {
 				cY = constrain(playerY, SCREEN_HEIGHT / 2 + bounds[level][1], bounds[level][3] - SCREEN_HEIGHT / 2);
 			}
 			oLvl = level;
+			dcolCheck = false;
 			hasStarted = true;
 		}
-		oX = playerX;
-		oY = playerY;
-		playerYVel += 0.3;
-		if (kHeld & KEY_DOWN) playerYVel += 0.9;
-		if (playerYVel > 9) playerYVel = 9;
-		if (playerYVel < -9) playerYVel = -9;
-		playerXVel = playerXVel / sqrt(6);
-		if (kHeld & KEY_LEFT) playerXVel -= 3;
-		if (kHeld & KEY_RIGHT) playerXVel += 3;
-		playerX += playerXVel;
-		hcolCheck = false;
-		if (vcolCheck) playerY -= abs(playerXVel);
-		for (curLine = levelStart[level]; curLine < levelEnd[level]; curLine++) {
-			if (lineCircle(linelist[curLine]->startX, linelist[curLine]->startY, linelist[curLine]->endX, linelist[curLine]->endY, playerX, playerY, 10)) {
-				hcolCheck = true;
-				break;
-			}
-		}
-		while (hcolCheck) {
-			tcolCheck = false;
+		for (int lol = 0; lol < SUBSTEPS; lol++) {
+			oX = playerX;
+			oY = playerY;
+			playerYVel += 0.3 / SUBSTEPS;
+			if (kHeld & KEY_DOWN) playerYVel += 0.9 / SUBSTEPS;
+			if (playerYVel > MAX_SPEED) playerYVel = MAX_SPEED;
+			if (playerYVel < -MAX_SPEED) playerYVel = -MAX_SPEED;
+			playerXVel /= xDiv;
+			if (kHeld & KEY_LEFT) playerXVel -= xAdd;
+			if (kHeld & KEY_RIGHT) playerXVel += xAdd;
+			playerX += playerXVel / SUBSTEPS;
+			hcolCheck = false;
+			if (vcolCheck) playerY -= abs(playerXVel/SUBSTEPS);
 			for (curLine = levelStart[level]; curLine < levelEnd[level]; curLine++) {
 				if (lineCircle(linelist[curLine]->startX, linelist[curLine]->startY, linelist[curLine]->endX, linelist[curLine]->endY, playerX, playerY, 10)) {
-					tcolCheck = true;
-					break;
+					if (abs((closestY - playerY) / (closestX - playerX))>0.84) continue;
+					hcolCheck = true;
+					if (playerX == closestX) continue;
+					else playerX = closestX + (closestX < playerX ? 10 : -10) * abs(cos(atan((playerY - closestY)/(playerX - closestX))));
 				}
 			}
-			if (tcolCheck) {
-				playerX -= 0.1 * (playerX < closestX ? 1 : -1);
-			} else break;
-		}
-		if (vcolCheck) playerY += abs(playerXVel);
-		tcolCheck = vcolCheck;
-		vcolCheck = false;
-		if (hcolCheck) playerXVel = 0;
-		playerY += playerYVel;
-		for (curLine = levelStart[level]; curLine < levelEnd[level]; curLine++) {
-			if (abs((linelist[curLine]->startY-linelist[curLine]->endY)/(linelist[curLine]->startX-linelist[curLine]->endX))>3) continue;
-			if (lineCircle(linelist[curLine]->startX, linelist[curLine]->startY, linelist[curLine]->endX, linelist[curLine]->endY, playerX, playerY, 10)) {
-				vcolCheck = (closestX == playerX) || abs((closestY - playerY) / (closestX - playerX))>0.5;
-				break;
-			}
-		}
-		if (tcolCheck) {
-			playerY += abs(playerXVel);
+			if (vcolCheck) playerY += abs(playerXVel/SUBSTEPS);
+			tcolCheck = vcolCheck;
+			vcolCheck = false;
+			playerY += playerYVel / SUBSTEPS;
 			for (curLine = levelStart[level]; curLine < levelEnd[level]; curLine++) {
 				if (abs((linelist[curLine]->startY-linelist[curLine]->endY)/(linelist[curLine]->startX-linelist[curLine]->endX))>3) continue;
 				if (lineCircle(linelist[curLine]->startX, linelist[curLine]->startY, linelist[curLine]->endX, linelist[curLine]->endY, playerX, playerY, 10)) {
 					vcolCheck = (closestX == playerX) || abs((closestY - playerY) / (closestX - playerX))>0.5;
-					break;
-				}
-			}
-			if (!vcolCheck) {
-				playerY -= abs(playerXVel);
-			}
-		}
-		while (vcolCheck) {
-			tcolCheck = false;
-			for (curLine = levelStart[level]; curLine < levelEnd[level]; curLine++) {
-				if (abs((linelist[curLine]->startY-linelist[curLine]->endY)/(linelist[curLine]->startX-linelist[curLine]->endX))>3) continue;
-				if (lineCircle(linelist[curLine]->startX, linelist[curLine]->startY, linelist[curLine]->endX, linelist[curLine]->endY, playerX, playerY, 10)) {
-					tcolCheck = (closestX == playerX) || abs((closestY - playerY) / (closestX - playerX))>0.5;
-					break;
+					if (!vcolCheck) continue;
+					if (playerX == closestX) playerY = closestY + (closestY < playerY ? 10 : -10);
+					else playerY = closestY + (closestY < playerY ? 10 : -10) * abs(sin(atan((playerY - closestY)/(playerX - closestX))));
+					if (closestY > playerY) jumpableFor = 25;
 				}
 			}
 			if (tcolCheck) {
-				playerY -= 0.1 * (playerY < closestY ? 1 : -1);
-			} else break;
-		}
-		if (vcolCheck) {
-			if ((kDown & KEY_A) && playerYVel > 0) {
-				playerYVel = -5 * ((kHeld&KEY_DOWN) ? 2 : 1);
-				vcolCheck = false;
-			} else {
-				vcolCheck = playerYVel > 0;
+				playerY += abs(playerXVel/SUBSTEPS);
+				for (curLine = levelStart[level]; curLine < levelEnd[level]; curLine++) {
+					if (abs((linelist[curLine]->startY-linelist[curLine]->endY)/(linelist[curLine]->startX-linelist[curLine]->endX))>3) continue;
+					if (lineCircle(linelist[curLine]->startX, linelist[curLine]->startY, linelist[curLine]->endX, linelist[curLine]->endY, playerX, playerY, 10)) {
+						tcolCheck = (closestX == playerX) || abs((closestY - playerY) / (closestX - playerX))>0.5;
+						vcolCheck = vcolCheck || tcolCheck;
+						if (!tcolCheck) continue;
+						if (playerX == closestX) playerY = closestY + (closestY < playerY ? 10 : -10);
+						else playerY = closestY + (closestY < playerY ? 10 : -10) * abs(sin(atan((playerY - closestY)/(playerX - closestX))));
+						if (closestY > playerY) jumpableFor = 25;
+					}
+				}
+				if (!vcolCheck) playerY -= abs(playerXVel/SUBSTEPS);
+			}
+			if (hcolCheck) playerXVel = 0;
+			if (vcolCheck) {
 				playerYVel = 0;
+			} else if (jumpableFor) jumpableFor--;
+			if (jumpableFor && (kDown & KEY_A)) {
+				playerYVel = xAdd * -6.5 * ((kHeld&KEY_DOWN) ? 2 : 1);
+				playerY -= abs(playerXVel);
+				jumpableFor = 0;
+			}
+			if (jumpableFor) rotSpd = ((playerX - oX) > 0 ? 0.1 : -0.1) * sqrt((playerX - oX) * (playerX - oX) + (playerY - oY) * (playerY - oY) * (jumpableFor ? 1 : 0));
+			else rotSpd /= pow(1.1, 1/13.0);
+			rot += rotSpd;
+			for (curLine = koStart[level]; curLine < koEnd[level]; curLine++) {
+				if (lineCircle(linelistKO[curLine]->startX, linelistKO[curLine]->startY, linelistKO[curLine]->endX, linelistKO[curLine]->endY, playerX, playerY, 10)) {
+					dcolCheck = true;
+					lol = SUBSTEPS + 1;
+					break;
+				}
 			}
 		}
 		cX += constrain(playerX, SCREEN_WIDTH / 2 + bounds[level][0], bounds[level][2] - SCREEN_WIDTH / 2) * 0.3;
 		cX /= 1.3;
 		cY += constrain(playerY, SCREEN_HEIGHT / 2 + bounds[level][1], bounds[level][3] - SCREEN_HEIGHT / 2) * 0.3;
 		cY /= 1.3;
-		if (vcolCheck) rotSpd = ((playerX - oX) > 0 ? 0.1 : -0.1) * sqrt((playerX - oX) * (playerX - oX) + (playerY - oY) * (playerY - oY) * (vcolCheck ? 1 : 0));
-		else rotSpd /= sqrt(1.1);
-		rot += rotSpd;
 		
 		// Start Render
 		C3D_FrameBegin(C3D_FRAME_SYNCDRAW);
 		C2D_TargetClear(top_main, clrWhite);
-		C2D_TargetClear(bottom, clrWhite);
+		//C2D_TargetClear(bottom, clrWhite);
 		
 		// Top Screen
 		C2D_SceneBegin(top_main);
@@ -217,26 +293,35 @@ int main(int argc, char* argv[]) {
 		// Draw Player
 		C2D_DrawCircleSolid(playerX - cX + SCREEN_WIDTH / 2,
 							playerY - cY + SCREEN_HEIGHT / 2,
-							0.3f, 10, clrBlack);
-		C2D_DrawCircleSolid(playerX + cos(rot) * 7 - cX + SCREEN_WIDTH / 2,
-							playerY + sin(rot) * 7 - cY + SCREEN_HEIGHT / 2,
-							0.4f, 2.5, C2D_Color32(255, 0, 0, 0xFF));
+							0.3f, (animID == -1 ? 10 : min(animTimer * 100, 10)), clrBlack);
+		C2D_DrawCircleSolid(playerX + cos(rot) * (animID == -1 ? 6 : min(animTimer * 60, 6)) - cX + SCREEN_WIDTH / 2,
+							playerY + sin(rot) * (animID == -1 ? 6 : min(animTimer * 60, 6)) - cY + SCREEN_HEIGHT / 2,
+							0.4f, (animID == -1 ? 2.5 : min(animTimer * 25, 2.5)), clrCyan);
+		
+		if (animID == 0) C2D_DrawRectSolid(0, 0, 0.9f, SCREEN_WIDTH, SCREEN_HEIGHT, C2D_Color32(0, 255, 255, C2D_FloatToU8(max(0, 1 - animTimer))));
 		
 		// Bottom Screne
-        C2D_SceneBegin(bottom);
+        //C2D_SceneBegin(bottom);
+		drawLog();
         
         // Done Rendering!
-		C3D_FrameEnd(0);
+		if (!dcolCheck) C3D_FrameEnd(0);
 		
-		if (playerY > bounds[level][3]) { //rip
+		/* ------------- AFTER FRAME CHECKS ------------- */
+		
+		// Death Check
+		if (playerY > bounds[level][3] || dcolCheck) {
+			playerY = constrain(playerY, bounds[level][1], bounds[level][3]);
+			playerX = constrain(playerX, bounds[level][0], bounds[level][2]);
 			// Start Render
-			C3D_FrameBegin(C3D_FRAME_SYNCDRAW);
+			if (!dcolCheck) C3D_FrameBegin(C3D_FRAME_SYNCDRAW);
 			C2D_TargetClear(top_main, clrWhite);
 			C2D_SceneBegin(top_main);
 			// Draw Level
 			drawLevel();
         	// Done Rendering!
 			C3D_FrameEnd(0);
+			animID = 1;
 			int frames = 0;
 			while (frames < 24) {
 				// Start Render
@@ -246,15 +331,51 @@ int main(int argc, char* argv[]) {
 				drawLevel();
 				// Funni Animation
 				C2D_DrawCircleSolid(playerX - cX + SCREEN_WIDTH / 2,
-									SCREEN_HEIGHT, 0.9f, (frames / 24.0f) * 69,
-									C2D_Color32(255, 200 - 200 * (frames / 24.0f), 0, C2D_FloatToU8(0.5 - (frames / 48.0f))));
+									playerY - cY + SCREEN_HEIGHT / 2, 0.9f, (frames / 24.0f) * 69,
+									C2D_Color32(255, 200 - 200 * (frames / 24.0f), 0,
+												C2D_FloatToU8(0.5 - (frames / 48.0f))));
 				C2D_DrawCircleSolid(playerX - cX + SCREEN_WIDTH / 2,
-									SCREEN_HEIGHT, 0.9f, (frames / 24.0f) * 69,
+									playerY - cY + SCREEN_HEIGHT / 2, 0.9f, (frames / 24.0f) * 69,
 									C2D_Color32(255, 255, 255, C2D_FloatToU8((frames / 12.0f) - 1)));
         		// Done Rendering!
 				C3D_FrameEnd(0);
 				frames++;
+				drawLog();
 			}
+			hasStarted = false;
+		}
+		
+		// Done Level Check
+		if (pointCircle(endPoint[level][0], endPoint[level][1], playerX, playerY, 10)) {
+			animID = 0;
+			int frames = 0;
+			float x = max(endPoint[level][0] - cX + SCREEN_WIDTH / 2, SCREEN_WIDTH - (endPoint[level][0] - cX + SCREEN_WIDTH / 2));
+			float y = max(endPoint[level][1] - cY + SCREEN_HEIGHT / 2, SCREEN_HEIGHT - (endPoint[level][1] - cY + SCREEN_HEIGHT / 2));
+			float dist = sqrt(x * x + y * y);
+			while (2 + frames * 10 < dist) {
+				// Start Render
+				C3D_FrameBegin(C3D_FRAME_SYNCDRAW);
+				C2D_SceneBegin(top_main);
+				// Draw Level
+				drawLevel();
+				// Draw Player
+				C2D_DrawCircleSolid(playerX - cX + SCREEN_WIDTH / 2,
+									playerY - cY + SCREEN_HEIGHT / 2,
+									0.3f, 10, clrBlack);
+				C2D_DrawCircleSolid(playerX + cos(rot) * 6 - cX + SCREEN_WIDTH / 2,
+									playerY + sin(rot) * 6 - cY + SCREEN_HEIGHT / 2,
+									0.4f, 2.5, clrCyan);
+				// Funni Animation
+				C2D_DrawCircleSolid(endPoint[level][0] - cX + SCREEN_WIDTH / 2,
+									endPoint[level][1] - cY + SCREEN_HEIGHT / 2,
+									0.9f, 2 + frames * 10, clrCyan);
+        		// Done Rendering!
+				C3D_FrameEnd(0);
+				frames++;
+				drawLog();
+			}
+			level ++;
+			if (level >= levels) level = 0;
 			hasStarted = false;
 		}
 	}
